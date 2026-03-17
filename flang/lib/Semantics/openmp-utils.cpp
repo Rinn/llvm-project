7d3379ca600347797a945feb6270963def5d5a5d
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2026-03-17 13:08:46 -0500

    [flang][OpenMP] Allow "Reason" messages to not have source locations
    When explanatory messages are generated there may be cases when there
    is no satisfactory source location to apply them to. This patch allows
    storing such messages without a source location.
    The messages will be equipped with a source location at the time when
    they are attached to the main error message (usually it will be the
    same location as used for the main message).
    
    Issue: https://github.com/llvm/llvm-project/issues/185287

--- flang/lib/Semantics/openmp-utils.cpp
+++ flang/lib/Semantics/openmp-utils.cpp
@@ -532,8 +532,20 @@ MaybeExpr MakeEvaluateExpr(const parser::OmpStylizedInstance &inp) {
       instance.u);
 }
 
-parser::Message &Reason::AttachTo(parser::Message &msg) {
-  msgs.AttachTo(msg);
+parser::Message &Reason::AttachTo(
+    parser::CharBlock source, parser::Message &msg) {
+  parser::Messages sourced;
+  for (auto &&msg : msgs.messages()) {
+    if (unsourced_.contains(&msg)) {
+      llvm::StringRef fmt{"%s"};
+      sourced.Say(source,
+          parser::MessageFixedText(fmt.data(), fmt.size(), msg.severity()),
+          msg.ToString());
+    } else {
+      sourced.Say(std::move(msg));
+    }
+  }
+  sourced.AttachTo(msg);
   return msg;
 }
 
